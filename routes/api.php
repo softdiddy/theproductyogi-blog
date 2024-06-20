d3f9eee0e42325ec78156173ac3952009ed53e32
Mohammed Lawal Abubakar <softdiddy@gmail.com>
2024-06-20 20:20:07 +0100

    protecting API route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,14 @@
-<?php
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\PostController;
 use App\Http\Controllers\Api\CommentController;
+use Illuminate\Support\Facades\Route;
 
-Route::get('posts', [PostController::class, 'index']);
-Route::post('posts', [PostController::class, 'store']);
-Route::post('posts/{post}/comments', [CommentController::class, 'store']);
+Route::post('register', [AuthController::class, 'register']);
+Route::post('login', [AuthController::class, 'login']);
+Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('posts', [PostController::class, 'index']);
+    Route::post('posts', [PostController::class, 'store']);
+    Route::post('posts/{post}/comments', [CommentController::class, 'store']);
+});
